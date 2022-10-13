69812573e08b255d3906ef396b68a4ba4df5b5c1
Billy Fletcher <Billyfletcher99@gmail.com>
2022-10-13 11:20:03 -0400

    first commit 10.13

--- src/App.js
+++ src/App.js
@@ -1,23 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import Logo from './assets/logo.png';
+import {
+  FaBars,
+} from 'react-icons/fa';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='fixed w-full h-[80px] flex justify-between items-center px-4 bg-[#0a192f] text-gray-300'>
+      <div>
+        <img src={Logo} alt='Logo Image' style={{ width: '50px' }} />
+      </div>
+
+      {/* navbar */}
+      <div>
+      <ul className='flex'>
+          <li className='px-4 cursor-pointer'>home</li>
+          <li className='px-4 cursor-pointer'>About</li>
+          <li className='px-4 cursor-pointer'>Skills</li>
+          <li className='px-4 cursor-pointer'>My work</li>
+          <li className='px-4 cursor-pointer'>Contact</li>
+          
+        </ul>
+      </div>
+
+      {/* hamburger */}
+      <div className="hidden">
+      <FaBars />
+      </div>
+
+      {/* mobile */}
+      <ul className="hidden">
+          <li>home</li>
+          <li>About</li>
+          <li>Skills</li>
+          <li>My work</li>
+          <li>Contact</li>
+      </ul>
+
+
+      {/* socials */}
+      <div className='hidden'></div>
+
+
     </div>
   );
 }
---
+++ src/components/navbar.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const navbar = () => {
+  return (
+    <h1 className='fixed w-full h-[80px] flex justify-between items-center px-4 bg-[#0a192f]'> hello</h1>
+  )
+}
+
+export default navbar
