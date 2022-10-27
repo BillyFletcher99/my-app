7eb61f4bda8672014ec4e9693505df6339319e22
Billy Fletcher <Billyfletcher99@gmail.com>
2022-10-27 19:19:59 -0400

    hopefully going through

--- src/App.js
+++ src/App.js
@@ -1,22 +1,11 @@
-import Navbar from "./components/Navbar";
-import Main from "./components/Main";
-import About from "./components/About";
-import Skills from "./components/Skills";
-import Work from "./components/Work";
-import Contact from "./components/Contact";
+
+
 
 
 
 function App() {
   return (
-    <div>
-      <Navbar />
-      <Main />
-      <About />
-      <Skills />
-      <Work />
-      <Contact />
-    </div>
+    <div></div>
   );
 }
 
